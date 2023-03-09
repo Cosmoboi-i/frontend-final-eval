d0a5f21aeb8561b05beb9095b8613762b6084c58
Aganya Bajaj <aganyabajaj7@gmail.com>
2023-03-09 23:04:29 +0530

    feat: create login component without functionality

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './login.css';
 
-export default function Login({ isRegister }) {
+export default function Login({ isRegister = false }) {
   return (
     <div className="login">
       <h1 className="heading">{isRegister ? 'Register to your CMS+ account' : 'Login to your CMS+ account'}</h1>
+      <div className="input-container">
+        <div className="input">
+          <div className="label">Email</div>
+          <input type="email" className="login-input" />
+        </div>
+        <div className="input">
+          <div className="label">Password</div>
+          <input type="password" className="login-input" />
+        </div>
+        <button className="login-button" onClick={handleClick}>
+          {isRegister ? 'Register' : 'Login'}
+        </button>
+        <div className="forgot-password">Forgot password?</div>
+      </div>
     </div>
   );
 }
--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,2 +1,4 @@
 export const HOME_ROUTE = "/";
 export const ERROR_ROUTE = "/error";
+export const VALIDATE_ROUTE = '/validate';
+export const AUTH_ROUTE = 'http://localhost:8888';
